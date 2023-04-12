5e2ce869bb0a268f55c303a51ca12bd18c0dc1f6
TomCastle69 <130562789+TomCastle69@users.noreply.github.com>
2023-04-12 13:51:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Döner mit Pommes')
+streamlit.text('Milchshake')
+streamlit.text('Cola XXL')
